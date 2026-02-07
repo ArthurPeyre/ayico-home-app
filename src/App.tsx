c9912a4c63cd18a382ce37aca7a66eb42bf2ef19
ArthurPeyre <peyre.arthur@outlook.fr>
2026-02-07 14:46:27 +0100

    Refactor: Clean up default projetc

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { getUsers } from './api/users'
 import type { User } from './types/user'
 
 
 function App() {
-  const [count, setCount] = useState(0)
   const [users, setUsers] = useState<User[]>([])
 
   const fetchUsers = async () => {
